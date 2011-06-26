1343b127117a11dddce8bf95dba999df06285759
Eric Kever <lolbummer@gmail.com>
2011-06-25 20:55:29 -0400

    Improved header system, added support for CC and BCC.

--- scripts/mail.php
+++ scripts/mail.php
@@ -1,15 +1,22 @@
 <?php
 	
 	// Configuration Settings
-	$mail_to = "youremail@domain.com";
+	$config['to'][] = 'youremail@example.com';
+	$config['to'][] = 'otheremail@example.com';
+	$config['cc'][] = 'cc1@example.com';
+	$config['cc'][] = 'cc2@example.com';
+	$config['bcc'][] = 'bcc1@example.com';
+	$config['bcc'][] = 'bcc2@example.com';
 	$subject = "Contact Form Submission";
 	$from = "contact@sitename.com";
 	
 	// Basic header information
-	$header = "From: <$from>\n";
-	$header .= "Return-path: <$from>\n";
-	$header .= "X-Sender-IP: " .$_SERVER['REMOTE_ADDR']."\n";
-	$header .= "Content-Type: text/html; \n charset=iso-8859-1 \n";
+	$headers = array(
+		'From' =>  "<$from>",
+		'Return-path' => "<$from>",
+		'X-Sender-IP' => $_SERVER['REMOTE_ADDR'],
+		'Content-Type' => 'text/html; \n charset=iso-8859-1 '
+	);
 	
 	// Construct the basic HTML for the message
 	$head = "<html><body>";
@@ -28,11 +35,14 @@
 
 	// Combine all the information
 	$body = "$head $table_start $text $table_end $info $foot";
+	
+	if(isset($config['cc']) && count($config['cc'])) $headers['Cc'] = implode(',', $config['cc']);
+	if(isset($config['bcc']) && count($config['bcc'])) $headers['Bcc'] = implode(',', $config['bcc']);
 
 	// If everything is filled out correctly, send the e-mail
 	if ($body != "") {
 		echo 'Your message sent successfully. Thank you for contacting us.';
-		mail($mail_to, $subject, $body, $header);
+		mail(implode(',', $config['mail']), $subject, $body, implode("\n", $headers));
 	}
 
 ?>
